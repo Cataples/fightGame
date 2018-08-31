cb8fc76451b52ca6f9d59747f9e301c14b414e12
Dana <danacalcaianu@yahoo.com>
2018-08-31 11:54:14 +0300

    added login

--- src/js/components/login.js
+++ src/js/components/login.js
@@ -1,5 +1,39 @@
-import React from "react";
+/* eslint: disable */
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import loginService from "../services/loginService";
 
-const Login = () => <h1>Login component</h1>;
+class LoginForm extends Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "title test",
+        };
+        this.handleSubmit = this.handleSubmit.bind( this );
+    }
 
-export default Login;
+    handleSubmit( event ) {
+        event.preventDefault();
+        loginService.loginService( this.username.value, this.password.value );
+        // this.props.history.push( "/dashboard" );
+    }
+    render() {
+        return (
+            <form onSubmit={ this.handleSubmit } className="login-form">
+                <label>Username
+                    <input type="text" name="username" ref={node => (this.username = node)} />
+                </label>
+                <label control="">Password
+                    <input type="password" password="password" ref={node => (this.password = node)} />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        );
+    }
+}
+
+LoginForm.PropTypes = {
+    history: PropTypes.object.isRequired, // eslint-disable-line
+};
+
+export default LoginForm;
---
+++ src/js/services/loginService.js
@@ -0,0 +1,15 @@
+import { sendPostRequest } from "./fetchRequest";
+
+const loginService = ( username, password ) => {
+    const path = "";
+    const body = {
+        username,
+        password,
+    };
+    console.log( body );
+    sendPostRequest( path, body );
+};
+
+export default {
+    loginService,
+};
